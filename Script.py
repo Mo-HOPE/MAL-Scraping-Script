8d48539dea47762e0a03968a3890a385a9413e8b
Mohamed AbdulRaouf <120792018+Mo-HOPE@users.noreply.github.com>
2023-06-18 19:04:23 +0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,3 +1,4 @@
+from ast import Try
 import requests
 import csv
 from bs4 import BeautifulSoup
@@ -16,103 +17,107 @@ def main():
 
     user_input()
 
-    # Get Anime Main Name
-    Anime_Name = soup.find("h1",{"class": "title-name"}).find("strong").text.strip()
+    try:
 
-    # Check For Alternative Name And Get It IF It Exist (print empty string if not)
-    if (soup.find("p",{"class": "title-english"}) == None):
-        Other_Name = ""
-    else:
-        Other_Name = soup.find("p",{"class": "title-english"}).text.strip()
-    
+        # Get Anime Main Name
+        Anime_Name = soup.find("h1",{"class": "title-name"}).find("strong").text.strip()
 
-    Anime_Information = soup.find_all("div",{"class": "spaceit_pad"}) # A list with all divs that contain anime informaion
-    start_point = 0 
-    
-    # Determine the div we start from
-    for i in range(len(Anime_Information)):
-        if (Anime_Information[i].find("span").text.strip() == "Type:"):
-            start_point = i
-            break
+        # Check For Alternative Name And Get It IF It Exist (print empty string if not)
+        if (soup.find("p",{"class": "title-english"}) == None):
+            Other_Name = ""
+        else:
+            Other_Name = soup.find("p",{"class": "title-english"}).text.strip()
+        
 
-
-    # divs with constant order in every anime type
-    Anime_Type = Anime_Information[start_point].find("a").text.strip()
-    Anime_Episodes = Anime_Information[start_point+1].contents[2].strip()
-    Anime_Status = Anime_Information[start_point+2].contents[2].strip()
-    Anime_Aired = Anime_Information[start_point+3].contents[2].strip()
-    Anime_Premiered = Anime_Information[start_point+4].find("a").text.strip()
+        Anime_Information = soup.find_all("div",{"class": "spaceit_pad"}) # A list with all divs that contain anime informaion
+        start_point = 0 
+        
+        # Determine the div we start from
+        for i in range(len(Anime_Information)):
+            if (Anime_Information[i].find("span").text.strip() == "Type:"):
+                start_point = i
+                break
 
 
-    # Determine a new start point for other divs depends on the anime type (order of divs change only in cases inside if condition)
-    if ((Anime_Type == "OVA") or (Anime_Type == "ONA") or (Anime_Type == "Special") or (Anime_Type == "Movie")):
-        new_start_point = start_point+6
-        Anime_Premiered = Anime_Aired
-    else:
+        # divs with constant order in every anime type
+        Anime_Type = Anime_Information[start_point].find("a").text.strip()
+        Anime_Episodes = Anime_Information[start_point+1].contents[2].strip()
+        Anime_Status = Anime_Information[start_point+2].contents[2].strip()
+        Anime_Aired = Anime_Information[start_point+3].contents[2].strip()
         Anime_Premiered = Anime_Information[start_point+4].find("a").text.strip()
-        new_start_point = start_point+8 
-    
-
-    # Determine anime studio with new start point
-    Anime_Studio = "" 
-    studios_list =  Anime_Information[new_start_point].find_all("a")
-    for i in range(len(studios_list)):
-        Anime_Studio = f"{Anime_Studio}, {studios_list[i].text.strip()}" 
-
-    Anime_Studio = Anime_Studio[2:]
-
-    # Determine anime source
-    Anime_Source = Anime_Information[new_start_point+1].contents[2].strip()
-
-    # Determine anime genres
-    genre_list = Anime_Information[new_start_point+2].find_all("span",{"itemprop": "genre"})
-    Anime_Genre = ""
-    for i in range(len(genre_list)):
-        Anime_Genre = f"{Anime_Genre}, {genre_list[i].text.strip()}"
-    Anime_Genre = Anime_Genre[2:]
 
 
-    # Determine anime rate
-    Anime_Rate = ""
-    for i in range(len(Anime_Information)):
-        if (Anime_Information[i].find("span").text.strip() == "Rating:"):
-              Anime_Rate = Anime_Information[i].contents[2].strip()
-              break
-
-    
-    # List of one dictionary that contain the informations of the anime as values 
-    Full_Info = [{"Name": Anime_Name, "Another Name": Other_Name,"Type": Anime_Type, 
-                 "Episodes": Anime_Episodes, "Status": Anime_Status, "Aired": Anime_Aired,
-                 "Premiered": Anime_Premiered, "Studio": Anime_Studio,"Source": Anime_Source, 
-                 "Genres": Anime_Genre,"Rating": Anime_Rate},]
-
-    
-    # Create Results folder if not exist in the same script folder to store csv files
-    code_file_directory = path.dirname(path.abspath(__file__))
-    folder_path = path.join(code_file_directory, "Results")
-
-    if not path.exists(folder_path):
-        mkdir(folder_path)
-
-    # Determine file name with the same name of anime and remove invalid characters
-    file_name = Anime_Name
-    invalid_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
-    for i in invalid_chars:
-        for j in file_name:
-            if (j == i):
-                file_name = file_name.replace(i,"")
-    file_path = folder_path+"/"+file_name+".csv"
-    
-    # create the csv file and but the keys and values of dic in it
-    with open(file_path,'w', encoding="utf-8-sig") as final_file:
-        keys = Full_Info[0].keys()
-        dict_writer = csv.DictWriter(final_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(Full_Info)
+        # Determine a new start point for other divs depends on the anime type (order of divs change only in cases inside if condition)
+        if ((Anime_Type == "OVA") or (Anime_Type == "ONA") or (Anime_Type == "Special") or (Anime_Type == "Movie")):
+            new_start_point = start_point+6
+            Anime_Premiered = Anime_Aired
+        else:
+            Anime_Premiered = Anime_Information[start_point+4].find("a").text.strip()
+            new_start_point = start_point+8 
         
-        
-        print("File Created Successfully") #end of the program
 
+        # Determine anime studio with new start point
+        Anime_Studio = "" 
+        studios_list =  Anime_Information[new_start_point].find_all("a")
+        for i in range(len(studios_list)):
+            Anime_Studio = f"{Anime_Studio}, {studios_list[i].text.strip()}" 
+
+        Anime_Studio = Anime_Studio[2:]
+
+        # Determine anime source
+        Anime_Source = Anime_Information[new_start_point+1].contents[2].strip()
+
+        # Determine anime genres
+        genre_list = Anime_Information[new_start_point+2].find_all("span",{"itemprop": "genre"})
+        Anime_Genre = ""
+        for i in range(len(genre_list)):
+            Anime_Genre = f"{Anime_Genre}, {genre_list[i].text.strip()}"
+        Anime_Genre = Anime_Genre[2:]
+
+
+        # Determine anime rate
+        Anime_Rate = ""
+        for i in range(len(Anime_Information)):
+            if (Anime_Information[i].find("span").text.strip() == "Rating:"):
+                Anime_Rate = Anime_Information[i].contents[2].strip()
+                break
+
+        
+        # List of one dictionary that contain the informations of the anime as values 
+        Full_Info = [{"Name": Anime_Name, "Another Name": Other_Name,"Type": Anime_Type, 
+                    "Episodes": Anime_Episodes, "Status": Anime_Status, "Aired": Anime_Aired,
+                    "Premiered": Anime_Premiered, "Studio": Anime_Studio,"Source": Anime_Source, 
+                    "Genres": Anime_Genre,"Rating": Anime_Rate},]
+
+        
+        # Create Results folder if not exist in the same script folder to store csv files
+        code_file_directory = path.dirname(path.abspath(__file__))
+        folder_path = path.join(code_file_directory, "Results")
+
+        if not path.exists(folder_path):
+            mkdir(folder_path)
+
+        # Determine file name with the same name of anime and remove invalid characters
+        file_name = Anime_Name
+        invalid_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
+        for i in invalid_chars:
+            for j in file_name:
+                if (j == i):
+                    file_name = file_name.replace(i,"")
+        file_path = folder_path+"/"+file_name+".csv"
+        
+        # create the csv file and but the keys and values of dic in it
+        with open(file_path,'w', encoding="utf-8-sig") as final_file:
+            keys = Full_Info[0].keys()
+            dict_writer = csv.DictWriter(final_file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(Full_Info)
+            
+            
+            print("File Created Successfully") #end of the program
+
+    except:
+        print("There is a problem with the url you enter please try again and enter a valid naime name")
 
 def user_input():
 
